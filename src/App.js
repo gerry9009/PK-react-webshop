07daf71aad03de7a9e0b9d67d35e85f30296bfeb
Gergo Buglyo <buglyogergo@gmail.com>
2024-03-03 14:33:08 +0100

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React from "react";
 import "./App.css";
 
+import banner1 from "./assets/img/banner-1.jpeg";
+import banner2 from "./assets/img/banner-2.jpeg";
+import banner3 from "./assets/img/banner-3.jpeg";
+
+const product = { title: "Samsung XX", price: 500, stock: 10 };
+const product1 = { title: "Iphone X", price: 600, stock: 2 };
+const product2 = { title: "Xiaomi", price: 400, stock: 7 };
+
 function App() {
+  function Card(props) {
+    const { name, price, stock } = props;
+
+    return (
+      <div className="card">
+        <div className="card-title">
+          <h2>{name}</h2>
+        </div>
+        <div className="card-body">
+          <p>{price} $</p>
+          <p>{stock} darab van készleten</p>
+          <button>Kosárhoz</button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>Hello PK</h1>
+      <h1>PK Webshop</h1>
+      <Card name={product.title} price={product.price} stock={product.stock} />
+      <Card
+        name={product1.title}
+        price={product1.price}
+        stock={product1.stock}
+      />
+      <Card
+        name={product2.title}
+        price={product2.price}
+        stock={product2.stock}
+      />
     </div>
   );
 }
